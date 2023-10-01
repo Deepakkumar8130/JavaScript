44789a6c1a1b722547ed9e1fdde500d689320d09
Deepakkumar8130 <dk0802270@gmail.com>
2023-10-01 05:39:19 +0530

    Assessment01-ImageThumbnail

---
+++ Assessment01/Assessment01/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imgElements = document.querySelectorAll(".img");
+
+    imgElements.forEach(function (imgElement) {
+        imgElement.addEventListener("mouseenter", function () {
+            var imgSrc = this.querySelector("img").getAttribute("src");
+
+            var fimage = document.getElementById("fimage");
+
+            var newImage = document.createElement("img");
+
+            newImage.src = imgSrc;
+            var fimageWidth = fimage.clientWidth;
+            var fimageHeight = fimage.clientHeight;
+
+            newImage.style.width = fimageWidth + "px";
+            newImage.style.height = fimageHeight + "px";
+
+            fimage.appendChild(newImage);
+        });
+
+        imgElement.addEventListener("mouseleave", function () {
+            var fimage = document.getElementById("fimage");
+            while (fimage.firstChild) {
+                fimage.removeChild(fimage.firstChild);
+            }
+        });
+    });
+});
+
